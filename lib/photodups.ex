e8fbb3e26d2ee9875626c36256cf7549f83be33f
castengo <catalina.astengo@hotmail.com>
2017-09-10 14:45:17 -0600

    initial app

---
+++ lib/photodups.ex
@@ -0,0 +1,18 @@
+defmodule Photodups do
+  @moduledoc """
+  Documentation for Photodups.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Photodups.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/photodups_test.exs
@@ -0,0 +1,8 @@
+defmodule PhotodupsTest do
+  use ExUnit.Case
+  doctest Photodups
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
