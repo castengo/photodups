874703d501e92a9c934dc5d587ba1d9976714c90
castengo <catalina.astengo@hotmail.com>
2017-09-10 17:07:08 -0600

    some test passing

--- lib/photodups.ex
+++ lib/photodups.ex
@@ -3,16 +3,32 @@ defmodule Photodups do
   Documentation for Photodups.
   """
 
-  @doc """
-  Hello world.
+  def scan(dir, dest) do
+    dir
+    |> list_files()
+    |> process(dest)
+  end
 
-  ## Examples
+  def process([file | files], dest) do
+    
+  end
 
-      iex> Photodups.hello
-      :world
+  def list_files(dir) do
+    path = dir <> "*.{jpg,JPG}"
+    IO.inspect path
+    Path.wildcard(path)
+  end
 
-  """
-  def hello do
-    :world
+  def read_date(path) do
+    {:ok, info} = Exexif.exif_from_jpeg_file(path)
+    do_read_date(info)
+  end
+
+  defp do_read_date(%{exif: %{datetime_original: date}}), do: date
+  defp do_read_date(_), do: nil
+
+  def read_size(path) do
+    File.stat!(path)
+    |> Map.get(:size)
   end
 end
--- test/photodups_test.exs
+++ test/photodups_test.exs
@@ -2,7 +2,21 @@ defmodule PhotodupsTest do
   use ExUnit.Case
   doctest Photodups
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "reads date created for image" do
+    assert "2014:12:01 19:50:49" == Photodups.read_date("test/fixtures/album/IMG_1751.JPG")
+  end
+
+  test "reads file size for image" do
+    assert 598247 == Photodups.read_size("test/fixtures/album/IMG_1751.JPG")
+  end
+
+  test "lists files for given directory" do
+    files = [
+      "test/fixtures/album/afraid.jpg",
+      "test/fixtures/album/IMG_1751.JPG",
+      "test/fixtures/album/IMG_5266.JPG"
+    ] |> Enum.sort
+
+    assert files == Photodups.list_files("test/fixtures/album/")
   end
 end
