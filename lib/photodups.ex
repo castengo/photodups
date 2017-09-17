b78dc685d8fdd97a315aaa4eafb7b73f5f1f41d4
castengo <catalina.astengo@hotmail.com>
2017-09-17 15:25:06 -0600

    Everything working, need to write tests

--- lib/photodups.ex
+++ lib/photodups.ex
@@ -5,80 +5,94 @@ defmodule Photodups do
 
   def report(dir) do
     all_files = list_files(dir)
-    final_files = scan(dir)
+    originals = scan(dir)
     total_files = Enum.count all_files
-    total_after = Enum.count final_files
-    removed = all_files -- final_files
+    total_after = Enum.count originals
+    duplicates = all_files -- originals
     IO.puts "Total files: #{total_files}"
     IO.puts "Total duplicates: #{total_files - total_after}"
     IO.puts "After dedup files: #{total_after}"
-    # IO.puts "FILES TO BE REMOVED:"
-    # Enum.each(removed, &IO.puts(&1))
   end
 
   def scan(dir) do
     dir
     |> list_files()
+    # get data if its there, if not mark as :skip
     |> Enum.map(&read_data(&1))
+    # reject files that don't have data
+    # |> Enum.reject(&(&1 == :skip))
+    # sort by date
     |> Enum.sort(&(elem(&1, 1) >= elem(&2, 1)))
-    |> IO.inspect()
+    # separate into list of list by date
     |> Enum.chunk_by(fn {_file, date, _width, _height} -> date end)
-    |> Enum.map(&remove_dups(&1))
+    # go through each list and pick the original and instagram files with highest rest
+    |> Enum.map(&process_chunk(&1))
+    |> List.flatten
+    # get only the file name
+    |> Enum.map(fn {file, _date, _width, _height} -> file end)
   end
 
-  @spec remove_dups([{String.t, String.t, non_neg_integer, non_neg_integer}]) :: String.t
-  def remove_dups(list) when length(list) > 1 do
-    list
-    |> Enum.sort(&((elem(&1, 2) * elem(&1, 3)) >= (elem(&2, 2) * elem(&2, 3))))
-    # |> print()
-    |> List.first()
-    |> elem(0)
+  @spec process_chunk([tuple]) :: [String.t]
+  def process_chunk(chunk) do
+    case List.first(chunk) do
+      {_, nil, _, _} -> chunk
+      _ -> remove_dups(chunk)
+    end
   end
-  def remove_dups([{file, _date, _width, _height}]), do: file
-  #
-  # def print(list) do
-  #   [chosen | others] = list
-  #   rejected = Enum.map(others, fn {file, _date, _width, _height} -> file end) |> Enum.join(", ")
-  #   IO.puts "#{rejected} are duplicates of #{elem(chosen, 0)}"
-  #   IO.puts "____________________________________"
-  #   list
-  # end
+
+  @spec remove_dups([{String.t, Setring.t, non_neg_integer, non_neg_integer}]) :: [String.t]
+  def remove_dups(list) do
+    #sort by highest resolution (height * width)
+    ordered_list = Enum.sort(list, &((elem(&1, 2) * elem(&1, 3)) >= (elem(&2, 2) * elem(&2, 3))))
+    original = Enum.find(ordered_list, [], fn {_file, _date, width, height} ->
+      width/height != 1
+    end)
+    instagram = Enum.find(ordered_list, [], fn {_file, _date, width, height} ->
+      width/height == 1
+    end)
+    # PRINT
+    duplicates = (ordered_list -- [original]) -- [instagram]
+    print(duplicates, original, instagram)
+    # PRINT
+    [original, instagram]
+  end
+
+  def print(dups, {original, _, _, _}, []) when length(dups) > 0 do
+    dups = Enum.map(dups, fn {file,_,_,_} -> Path.basename(file) end) |> Enum.join(", ")
+    orig = Path.basename(original)
+    IO.puts "**********DUPLICATES*******************"
+    IO.puts "#{dups} are duplicates of #{orig}"
+    IO.puts "---------------------------------------"
+  end
+  def print(dups, {original,_,_,_}, {instagram,_,_,_}) when length(dups) > 0 do
+    dups = Enum.map(dups, fn {file,_,_,_} -> Path.basename(file) end) |> Enum.join(", ")
+    orig = Path.basename(original)
+    inst = Path.basename(instagram)
+    IO.puts "**********DUPLICATES*******************"
+    IO.puts "#{dups} are duplicates of #{orig} and #{inst}"
+    IO.puts "---------------------------------------"
+  end
+  def print(_, _, _), do: nil
 
   def list_files(dir) do
     path = dir <> "*.{jpg,JPG}"
     Path.wildcard(path)
   end
 
-  @spec read_data(String.t) :: {String.t, non_neg_integer, non_neg_integer}
-  def read_data(path) do
-    path
+  @spec read_data(String.t) :: {String.t, non_neg_integer, non_neg_integer, non_neg_integer}
+  def read_data(file) do
+    file
     |> Exexif.exif_from_jpeg_file()
     |> case do
-      {:ok, info} -> {path, info}
-      {:error, _} -> {path, nil}
-      things -> {path, things}
+      {:ok, %{exif: %{datetime_original: date}}} -> {file, date}
+      _ -> {file, nil}
     end
-    |> get_date()
-    |> get_dimensions()
+    |> add_dimensions()
   end
 
-  @spec get_date({String.t, map | nil}) :: {String.t, non_neg_integer}
-  defp get_date({file, %{exif: %{datetime_original: date}}}) do
-    IO.puts "YES METADATA"
-    {file, date}
-  end
-  defp get_date({file, nil}) do
-    IO.puts "file #{file} didn't have metadata"
-    {file, "unknown_#{:rand.uniform(100000)}"}
-  end
-  defp get_date({file, things}) do
-    IO.puts "WHAT the hell is this?"
-    IO.inspect things
-    {file, "unknown_#{:rand.uniform(100000)}"}
-  end
-
-
-  defp get_dimensions({file, date}) do
+  @spec add_dimensions({String.t, String.t | nil}) :: {String.t, non_neg_integer, non_neg_integer, non_neg_integer}
+  defp add_dimensions({file, nil}), do: {file, nil, nil, nil}
+  defp add_dimensions({file, date}) do
     {_type, width, height, _info} = ExImageInfo.info File.read!(file)
     {file, date, width, height}
   end
